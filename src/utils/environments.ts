a0336f477eab644d7c4f65dea8bdb3f102ac7bf9
RazorPG <rafidhilmi0504@gmail.com>
2026-04-12 11:23:41 +0700

    fix: environment variable

--- src/utils/environments.ts
+++ src/utils/environments.ts
@@ -13,7 +13,8 @@ export const EMAIL_SMTP_PORT: number =
 export const EMAIL_SMTP_HOST: string = process.env.EMAIL_SMTP_HOST || ""
 export const EMAIL_SMTP_SERVICE_NAME: string =
   process.env.EMAIL_SMTP_SERVICE_NAME || ""
-export const CLIENT_HOST: string = process.env.CLIENT_HOST || ""
+export const CLIENT_HOST: string =
+  process.env.CLIENT_HOST || "http://localhost:3000"
 export const CLOUDINARY_CLOUD_NAME: string =
   process.env.CLOUDINARY_CLOUD_NAME || ""
 export const CLOUDINARY_API_KEY: string = process.env.CLOUDINARY_API_KEY || ""
